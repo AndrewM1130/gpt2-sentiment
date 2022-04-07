966b2753c03afdb850251a602be910161f8a3efa
Andrew Ma <andrewm2010796@gmail.com>
2022-04-07 14:27:12 -0700

    exploring python gui's for possible app deployment' :

---
+++ app/.#wx.py
@@ -0,0 +1,1 @@
+andrewma@processor1.local.27168
---
+++ app/wx.py
@@ -0,0 +1,9 @@
+
+# Python program to execute
+# main directly
+print ("Always executed")
+ 
+if __name__ == "__main__":
+    print ("Executed when invoked directly")
+else:
+    print ("Executed when imported")
